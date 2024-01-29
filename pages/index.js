0a38a461b2f6f0da1c4ce3628d51fa2afe2e50d9
vijay saroj <vijaysaroj4527@gmail.com>
2024-01-29 11:17:25 +0530

    new changes

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,55 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({ posts }) {
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const query = `
+          query getPosts {
+            posts {
+              nodes {
+                title
+                content
+              }
+            }
+          }
+        `;
+
+        const url = process.env.NEXT_PUBLIC_API_URL;
+
+        const res = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ query }),
+        });
+
+        if (!res.ok) {
+          console.error(`Error fetching data. Status: ${res.status}`);
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await res.json();
+        if (data) {
+          setPosts(data.data.posts.nodes);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setPosts([]);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
   return (
     <>
       <Head>
@@ -16,7 +59,7 @@ export default function Home({ posts }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-      {posts.map((post) => (
+        {posts.map((post) => (
           <div key={post.title}>
             <h2>{post.title}</h2>
             <div>{post.content}</div>
@@ -26,55 +69,3 @@ export default function Home({ posts }) {
     </>
   );
 }
-
-
-
-
-
-export async function getServerSideProps() {
-  try {
-    const query = `
-      query getPosts {
-        posts {
-          nodes {
-            title
-            content
-          }
-        }
-      }
-    `;
-
-    const url = process.env.NEXT_PUBLIC_API_URL;
-
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ query }),
-    });
-
-    if (!res.ok) {
-      console.error(`Error fetching data. Status: ${res.status}`);
-      throw new Error("Failed to fetch data");
-    }
-
-    const data = await res.json();
-    if (data) {
-      return {
-        props: {
-          posts: data.data.posts.nodes,
-        },
-      };
-    }
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return {
-      props: {
-        posts: [],
-      },
-    };
-  }
-}
-
-
